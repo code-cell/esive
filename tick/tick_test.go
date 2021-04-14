9be0bc524d24df75949d8fd61baf3dcc9e0139da
Albert Callarisa <albert@acroca.com>
2021-04-14 06:30:50 +0200

    Extended ticker time for tests

--- tick/tick_test.go
+++ tick/tick_test.go
@@ -10,18 +10,18 @@ import (
 )
 
 func TestTick(t *testing.T) {
-	tick := NewTick(10 * time.Millisecond)
+	tick := NewTick(100 * time.Millisecond)
 	require.Equal(t, int64(0), tick.Current())
 
 	go tick.Start()
-	time.Sleep(21 * time.Millisecond) // Force 2 ticks
+	time.Sleep(201 * time.Millisecond) // Force 2 ticks
 	tick.Stop()
 
 	require.Equal(t, int64(2), tick.Current())
 }
 
 func TestTickSubscribers(t *testing.T) {
-	tick := NewTick(10 * time.Millisecond)
+	tick := NewTick(100 * time.Millisecond)
 	startAt := time.Now()
 	called := false
 	tick.AddSubscriber(func(ctx context.Context, tick int64, dt time.Duration) {
@@ -29,24 +29,24 @@ func TestTickSubscribers(t *testing.T) {
 		require.WithinDuration(t, time.Now(), startAt.Add(dt), 2*time.Millisecond)
 		dl, ok := ctx.Deadline()
 		require.True(t, ok)
-		require.WithinDuration(t, time.Now().Add(10*time.Millisecond), dl, 2*time.Millisecond)
+		require.WithinDuration(t, time.Now().Add(100*time.Millisecond), dl, 20*time.Millisecond)
 		called = true
 	})
 	go tick.Start()
-	time.Sleep(11 * time.Millisecond) // Force 1 tick
+	time.Sleep(101 * time.Millisecond) // Force 1 tick
 	tick.Stop()
 	require.True(t, called)
 }
 
 func TestTickSubscribers_TakeTooLong_CallsAllTicksAnyway(t *testing.T) {
-	tick := NewTick(10 * time.Millisecond)
+	tick := NewTick(100 * time.Millisecond)
 	called := int32(0)
 	tick.AddSubscriber(func(ctx context.Context, tick int64, dt time.Duration) {
 		atomic.AddInt32(&called, 1)
 		time.Sleep(1000 * time.Millisecond)
 	})
 	go tick.Start()
-	time.Sleep(21 * time.Millisecond) // Force 2 ticks
+	time.Sleep(201 * time.Millisecond) // Force 2 ticks
 	tick.Stop()
 	require.Equal(t, int32(2), called)
 }
